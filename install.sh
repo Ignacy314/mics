ccfbd7f3e1961a11b8e07270b6cb0dfd79ee0031
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2024-12-05 18:09:33 +0100

    x

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ mkdir -p log
 git clone https://github.com/Ignacy314/mics
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt-get install -y libasound2-dev;
-sudo apt-get install -y libwebkit2gtk-4.0;
-sudo apt-get install cmake;
-sudo apt-get install gfortran;
+# sudo apt-get install -y libasound2-dev;
+# sudo apt-get install -y libwebkit2gtk-4.0;
+# sudo apt-get install cmake;
+# sudo apt-get install gfortran;
 cd mics
 $SHELL
--- src/data/imu.rs
+++ src/data/imu.rs
@@ -290,8 +290,8 @@ impl Imu {
 
     fn calculate_angle(mag: &[f32; 3], acc: &[f32; 3]) -> f32 {
         // Project mag onto a plane perpendicular to Earth's gravity vector
-        //let vec_north = Self::oproj(mag, acc);
-        let vec_north = mag;
+        let vec_north = Self::oproj(mag, acc);
+        //let vec_north = mag;
 
         // Assuming x is forward y is left
         vec_north[1].atan2(vec_north[0]) * 180.0 / PI
