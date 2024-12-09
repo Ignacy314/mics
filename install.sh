87cc14103da8ce8affbf995df3a36291d5c7141e
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2024-12-09 18:46:12 +0100

    x

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-mkdir -p ~/project
-cd ~/project
+sudo -i -u test bash << EOF
+mkdir -p \$HOME/project
+cd \$HOME/project
 mkdir -p data
 mkdir -p data/data
 mkdir -p data/i2s
@@ -11,14 +12,17 @@ rm -rf mics
 git clone https://github.com/Ignacy314/mics
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+cd mics
+ln -s \$HOME/project/mics/run.sh /usr/local/bin/andros
+EOF
+
+cp ANDROSi2s.dtbo /boot/firmware/overlays
+cp -f config.txt /boot/firmware/config.txt
+chmod +x run.sh
+
 apt-get install -y libasound2-dev;
 apt-get install -y libwebkit2gtk-4.0;
 # sudo apt-get install cmake;
 # sudo apt-get install gfortran;
-cd mics
-cp ANDROSi2s.dtbo /boot/firmware/overlays
-cp -f config.txt /boot/firmware/config.txt
-chmod +x run.sh
-ln -s ~/project/mics/run.sh /usr/local/bin/andros
 # $SHELL
 # cargo build -r
