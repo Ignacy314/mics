55ab9eef2737dc01ae2dc6a0a6f72607d7a9a58e
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-10 22:19:11 +0100

    x

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ echo -e '#!/bin/bash
 sleep 10
 echo \$(ip a s wlan0 | grep ether | egrep -o ..:..:..:..:..:.. | head -1) > \$HOME/andros/mac
 echo \$(ip -4 -o a | grep wlan | egrep -o "192\.168\.[0-9]{1,3}\.[0-9]{1,3}" | head -1) > \$HOME/andros/ip' > $HOME/save_mac_ip.sh
-(crontab -l 2>/dev/null; echo "@reboot $HOME/save_mac_ip.sh") | crontab -
 EOF
+# (crontab -l 2>/dev/null; echo "@reboot $HOME/save_mac_ip.sh") | crontab -
 
 chmod +x /home/test/save_mac_ip.sh
 cp /home/test/save_mac_ip.sh /etc/init.d/
