40c4d51ed480a8d65245b60927d12cd0bcdcb523
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-23 13:02:10 +0100

    x

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ while ! [ "$(ping -c 1 google.com)" ]; do
 done
 
 # cd $HOME/andros/andros && /usr/bin/git pull
-/usr/bin/git -C $HOME/andros/andros pull
+git -C $HOME/andros/andros pull
 
 # if [ $(cat $HOME/andros/andros/write_to_disk) = 1 ]; then
 #   $HOME/.cargo/bin/cargo install --path \$HOME/andros/andros --locked
@@ -20,4 +20,4 @@ $HOME/andros/andros/justfile
 sleep 2
 
 echo "start andros" > $HOME/andros_started
-while true; do /home/test/.cargo/bin/andros; sleep 5; done
+while true; do andros; sleep 5; done
