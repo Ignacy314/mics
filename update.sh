05a6958e2d003c9d5a0a5ff472d81c78c6ca0b73
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-23 13:35:56 +0100

    x

--- update.sh
+++ update.sh
@@ -6,15 +6,7 @@ while ! [ "$(ping -c 1 google.com)" ]; do
   sleep 1
 done
 
-# cd $HOME/andros/andros && /usr/bin/git pull
 git -C $HOME/andros/andros pull
-
-# if [ $(cat $HOME/andros/andros/write_to_disk) = 1 ]; then
-#   $HOME/.cargo/bin/cargo install --path \$HOME/andros/andros --locked
-# else
-#   $HOME/.cargo/bin/cargo install --path \$HOME/andros/andros --locked --no-default-features
-# fi
-
 just -f $HOME/andros/andros/justfile
 
 sleep 2
