4b6521a4b423d8e8a271e81dbcbba60c0cb0f7b1
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-14 21:47:58 +0100

    x

--- src/data/ina.rs
+++ src/data/ina.rs
@@ -24,7 +24,7 @@ impl Ina {
         Ok(Self {
             device: ina,
             voltage: CircularVec::<u32>::new(10 * 120),
-            bat_status: CircularVec::<i8>::new(100),
+            bat_status: CircularVec::<i8>::new(150),
             prev_charge: Charge::default(),
             parts: 3,
         })
