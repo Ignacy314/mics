63a805028620105e41685d6be28bcc1a3acbc0dc
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-15 18:27:50 +0100

    x

--- update.sh
+++ update.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+echo "update start"
 . $HOME/.profile
 . $HOME/.bashrc
 
+echo "waiting for internet connection..."
 while ! [ "$(ping -c 1 10.66.66.1)" ]; do
   sleep 1
 done
 
+echo "checking for updates"
 git -C $HOME/andros/andros pull
 just -f $HOME/andros/andros/justfile
 
 # sleep 2
 
+echo "starting andros"
 echo "start andros" > $HOME/started_andros
 while true; do andros; sleep 5; done
