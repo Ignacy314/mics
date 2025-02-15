c9f4ccb82de1306f03610036241bba5a3ed8937c
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-15 23:05:05 +0100

    x

--- src/audio.rs
+++ src/audio.rs
@@ -89,7 +89,7 @@ impl<'a> CaptureDevice<'a> {
             default => return Err(CaptureDeviceError::FormatUnimplemented(*default)),
         };
 
-        let mut buf = [0i32; 1024 * 1024];
+        let mut buf = [0i32; 1024 * 512];
         let wav_spec = hound::WavSpec {
             channels: self.channels as u16,
             sample_rate: self.samplerate,
